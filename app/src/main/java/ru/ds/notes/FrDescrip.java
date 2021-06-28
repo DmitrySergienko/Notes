aac2f1499404f7fb9d7eb744a63b627a68d3e005
DmitrySergienko <dmitrisergienko@gmail.com>
2021-06-28 19:16:33 +0300

    List items attached

--- app/src/main/java/ru/ds/notes/FrDescrip.java
+++ app/src/main/java/ru/ds/notes/FrDescrip.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 
 public class FrDescrip extends Fragment {
@@ -16,6 +17,7 @@ public class FrDescrip extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
@@ -23,5 +25,7 @@ public class FrDescrip extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fr_descrip, container, false);
+
+
     }
 }
---
+++ app/src/main/java/ru/ds/notes/MyExpandableListAdapter.java
@@ -0,0 +1,142 @@
+package ru.ds.notes;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+
+public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+
+    public MyExpandableListAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 3;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 3;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View groupView, ViewGroup parent) {
+        if (groupView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            groupView = inflater.inflate(R.layout.listview_group, null);
+        }
+
+        switch (groupPosition) {
+            case 0: {
+                ((TextView) groupView.findViewById(R.id.question)).setText("Group 1");
+            } break;
+
+            case 1: {
+                ((TextView) groupView.findViewById(R.id.question)).setText("Group 2");
+            } break;
+
+            case 2: {
+                ((TextView) groupView.findViewById(R.id.question)).setText("Group 3");
+            } break;
+        }
+
+        return groupView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View childView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        childView = inflater.inflate(R.layout.listview_child, null);
+
+        switch (groupPosition) {
+            case 0: {
+                switch (childPosition) {
+                    case 0: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 1 child 1");
+                    } break;
+
+                    case 1: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 1 child 2");
+                    } break;
+
+                    case 2: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 1 child 3");
+                    } break;
+                }
+            } break;
+
+            case 1: {
+                switch (childPosition) {
+                    case 0: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 2 child 1");
+                    } break;
+
+                    case 1: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 2 child 2");
+                    } break;
+
+                    case 2: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 2 child 3");
+                    } break;
+                }
+            } break;
+
+            case 2: {
+                switch (childPosition) {
+                    case 0: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 3 child 1");
+                    } break;
+
+                    case 1: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 3 child 2");
+                    } break;
+
+                    case 2: {
+                        ((TextView) childView.findViewById(R.id.answer)).setText("Group 3 child 3");
+                    } break;
+                }
+            } break;
+        }
+
+        return childView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
+
